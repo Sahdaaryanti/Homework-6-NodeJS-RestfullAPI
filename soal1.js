1e00e2b083e327481608fcae35eeb96b4062c2c0
Sahda Aryanti <11211080.student.itk.ac.id>
2023-09-29 23:09:13 +0800

    Adding Homework 6

---
+++ soal1.js
@@ -0,0 +1,17 @@
+//mengimpor modul 'rumus.js'
+const perhitungan = require('./perhitungan');
+
+//menentukan nilai sisi persegi dan menentukan panjang serta lebar persegi panjang
+let sisiPersegi = 3;
+let panjangRectangle = 8;
+let lebarRectangle = 6;
+
+// Menggunakan fungsi dari 'perhitungan.js' untuk menghitung dan mencetak luas dan keliling
+console.log("Luas Persegi dengan Sisi " + sisiPersegi + 
+    " Adalah " + perhitungan.hitungLuasPersegi(sisiPersegi));
+console.log("Keliling Persegi dengan Sisi " + sisiPersegi + 
+    " Adalah " + perhitungan.hitungKelilingPersegi(sisiPersegi));
+console.log("Luas Persegi Panjang dengan Panjang " + panjangRectangle + " dan Lebar " + lebarRectangle +
+    " Adalah " + perhitungan.hitungLuasRectangle(panjangRectangle, lebarRectangle));
+console.log("Keliling Persegi Panjang dengan Panjang " + panjangRectangle + " dan Lebar " +lebarRectangle +
+    " Adalah " + perhitungan.hitungKelilingRectangle(panjangRectangle, lebarRectangle));
